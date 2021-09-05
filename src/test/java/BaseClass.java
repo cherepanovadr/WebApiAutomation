2844a5ee6d14345cf60660448521a661348cab7d
darina.cherepanova <cherepanovadr@gmail.com>
2021-09-05 18:45:01 +0300

    beautify

--- src/test/java/BaseClass.java
+++ src/test/java/BaseClass.java
@@ -5,7 +5,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 import java.io.IOException;
-//test pull request
+
 public class BaseClass {
     protected static final String BASE_ENDPOINT = "https://api.github.com";
     CloseableHttpClient client;
